95cd5f764d549b63bf6b370b83a8823de232acd2
Niklas <nb@niklasbarning.de>
2025-08-12 22:12:18 +0200

    Update configuration to allow custom plugins and modify Gemfile.lock for dependency updates, including Jekyll and Liquid versions.

--- _plugins/taint_compat.rb
+++ _plugins/taint_compat.rb
@@ -9,4 +9,19 @@ unless nil.respond_to?(:tainted?)
   end
 end
 
+# Additionally, guard Liquid's taint check to avoid calling `tainted?` on nil
+begin
+  require 'liquid'
+  if Liquid.const_defined?(:Variable)
+    # Force-disable Liquid's taint check in dev environments where Ruby >= 3.2 removes tainted?
+    Liquid::Variable.class_eval do
+      def taint_check(_obj)
+        # no-op
+      end
+    end
+  end
+rescue LoadError
+  # Ignore if Liquid is not available yet; Jekyll will require it.
+end
+
 
